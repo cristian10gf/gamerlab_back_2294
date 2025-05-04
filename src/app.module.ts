f1b63e06655f5510a80e2c653b00e8b3dd338e1c
cristian1014 <cristiandavidgf@gmail.com>
2025-05-04 18:51:19 -0500

    feat: add Auth module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { EstudianteModule } from './modules/estudiante/estudiante.module';
 import { PrismaService } from './prisma/prisma.service'
 import { MateriaModule } from './modules/materia/materia.module';
 import { NrcModule } from './modules/nrc/nrc.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { NrcModule } from './modules/nrc/nrc.module';
     EstudianteModule,
     MateriaModule,
     NrcModule,
+    AuthModule,
   ],
   providers: [],
 })
