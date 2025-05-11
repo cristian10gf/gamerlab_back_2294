1d2b83dc5d172244d133d90dfd4d8bd87de262ac
cristian gonzalez franco <97616425+cristian10gf@users.noreply.github.com>
2025-05-11 21:21:21 +0000

    new method of deploy vercel

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppFactory } from './AppFactory';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const { appPromise } = AppFactory.create();
+  const app = await appPromise;
 
   app.setGlobalPrefix('api/v1');
 
